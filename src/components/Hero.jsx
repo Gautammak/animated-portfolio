d3e9c5fc2fd26a2be11a28e7e702e399a447d6ed
gautam-makavana-us <gautammakavana.logicwind@gmail.com>
2025-05-11 12:29:31 +0530

    Tc

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,13 @@
 import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer"; // NEW
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
-  const { ref, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.25,
-  });
-
   return (
-    <section className={`relative w-full h-screen mx-auto`} ref={ref}>
+    <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -25,14 +19,13 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Gautam</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop robust server-side logic, manage databases, and <br className='sm:block hidden' />
-            build scalable APIs.
+          I develop robust server-side logic, manage databases, and <br className='sm:block hidden' />
+          build scalable APIs.
           </p>
         </div>
       </div>
 
-      {/* ✅ Lazy-load the canvas only when it's in view */}
-      {inView && <ComputersCanvas />}
+      {/* <ComputersCanvas /> */}
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
